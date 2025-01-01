afed514327d99afe28f73baf0664d1161421d369
unknown <j.quabeck@gmx.net>
2025-01-01 12:47:58 +0100

    first test

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class LikeTests(APITestCase):
+
+    def test_get_like(self):
+        url = reverse('like-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_answer(self):
+        url = reverse('answer-list-create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
